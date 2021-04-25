038393332341cf0b3cce9004c46f9b67a80d5760
Yoshida Tsubasa <71028859+T-Yoshida00112@users.noreply.github.com>
2021-04-25 20:57:07 +0900

    スタート画面の作成
    スタート画面の作成とクイズへの遷移を作成しました。

--- app.js
+++ app.js
@@ -1,33 +1,51 @@
-const quiz = [
-    {
-        question : '私がよく飲む水は？',
-        answers : [
-                    'ボルビック',
-                    'クリスタルガイザー',
-                    'いろはす',
-                    '雨水'
-            ],
-        correct : 'クリスタルガイザー'
+//ゲームをスタートさせる
+const $startBtn = document.getElementById('quiz-btn');
+$startBtn.addEventListener('click', (e) => {
+    document.getElementById("quiz").classList.add("slidein");
+    document.getElementById("quiz").classList.remove("slideout");
+    setupQuiz();
+    return false;
+});
+
+//ゲームをリセットする
+const $resetBtn = document.getElementById('reset');
+$resetBtn.addEventListener('click', (e) => {
+    document.getElementById("quiz").classList.add("slideout");
+    document.getElementById("quiz").classList.remove("slidein");
+    resetQuiz();
+    return false;
+});
+//クイズの内容
+
+const quiz = [{
+        question: 'このクイズはなんのクイズ？',
+        answers: [
+            'ボルビック',
+            'クリスタルガイザー',
+            '水',
+            'おみず'
+        ],
+        correct: 'おみず'
     },
     {
-        question : '水の化学式は？',
-        answers : [
-                    'H₂O',
-                    'CO₂',
-                    'プラスチック',
-                    '目覚まし時計'
-            ],
-        correct : 'H₂O'
+        question: '水の化学式は？',
+        answers: [
+            'H₂O',
+            'CO₂',
+            'プラスチック',
+            '目覚まし時計'
+        ],
+        correct: 'H₂O'
     },
     {
-        question : '水が凍ると何になる？',
-        answers : [
-                    'おにぎり',
-                    '小売',
-                    'いろはす',
-                    '氷'
-            ],
-        correct : '氷'
+        question: '水が凍ると何になる？',
+        answers: [
+            'おにぎり',
+            '小売',
+            'いろはす',
+            '氷'
+        ],
+        correct: '氷'
     }
 ];
 
@@ -41,12 +59,18 @@ const buttonLength = $button.length;
 
 //クイズの問題文と選択肢を定義する
 const setupQuiz = () => {
-    document.getElementById('js-question').textContent = quiz[quizIndex].question;
+    document.getElementById('js-question').textContent = "Q" + (quizIndex + 1) + " " + quiz[quizIndex].question;
     for (let i = 0; i < buttonLength; i++) {
         $button[i].textContent = quiz[quizIndex].answers[i];
     }
 }
-setupQuiz();
+
+
+//クイズをリセットする
+const resetQuiz = () => {
+    quizIndex = 0;
+    score = 0;
+}
 
 
 //クリックされたら正誤判定をする
@@ -60,12 +84,13 @@ const clickHandler = (e) => {
 
     quizIndex++;
 
-    if(quizIndex < quizLength){
+    if (quizIndex < quizLength) {
         //問題数がまだあるときの処理
         setupQuiz();
-    }else{
+    } else {
         //問題数がもうないときの処理
         window.alert('終了!あなたの正解数は' + score + '/' + quizLength + 'です！');
+        quizIndex = 0;
     }
 }
 
